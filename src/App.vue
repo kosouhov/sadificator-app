fb65d17748d99dde0b680ffb9cccf61e878ba45a
kosouhov <vladimir@kosouhov.com>
2018-10-29 22:39:17 +0300

    add functionality

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <Upload
+      @chooseImage="changeFile"
+    />
+    <img :src="image" />
   </div>
 </template>
 
 <script>
+import Upload from './components/Upload.vue'
+
 export default {
   name: 'app',
+  components: {
+    Upload
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      image: ''
+    }
+  },
+  methods: {
+    changeFile: function(e) {
+      let files = e.target.files || e.dataTransfer.files
+      if (files.length) {
+        this.createImage(files[0])
+      }
+    },
+    createImage(file) {
+      let image = new Image(),
+        reader = new FileReader(),
+        o = this
+      reader.onload = (e) => {
+        o.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
     }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style lang="sass">
+  @import './sass/style.sass'
 </style>
---
+++ src/components/Upload.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="upload">
+		<div class="wrapper">
+			<div class="upload__header">
+				<div class="header">
+					<div class="header__title">Sadificator</div>
+					<div class="header__subtitle">Make your look sad</div>
+				</div>
+			</div>
+			<div class="upload__content">
+				<div class="content">
+					<div class="content__input">
+						<input type="file" id="input" @change="change">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Upload',
+    methods: {
+    	change: function(e) {
+    		this.$emit('chooseImage', e)
+    	}
+    }
+}
+</script>
+
+<style scoped lang="sass">
+	.upload
+		width: 100%
+		height: 100%
+		background: url(../assets/rain-e833b50f2d_1920.jpg) center center no-repeat
+		background-size: cover
+	.wrapper
+		display: flex
+		flex-direction: column
+		justify-content: space-between
+	.upload__header
+		display: flex
+		align-items: flex-end
+		height: 80px
+	.header
+		display: flex
+		align-items: baseline
+		justify-content: flex-start
+	.header__title
+		font-size: 29px
+		font-weight: 800
+		text-transform: uppercase
+	.header__subtitle
+		margin-left: 20px
+		font-size: 18px
+		font-weight: 300
+</style>
